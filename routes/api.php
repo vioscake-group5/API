73d53ef89f378beb8a4bd48287711da9c39bb79d
Dimas <124556325+Diz09@users.noreply.github.com>
2024-05-22 19:56:39 +0700

    edit tipis

--- routes/api.php
+++ routes/api.php
@@ -15,30 +15,45 @@ use App\Http\Controllers\KueController;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\PesananController;
 
+/*
+    how to run in handphone
+
+    hanya test API
+    - by testing (download app nya di playstore, cari API Tester)
+    - cmd -> ipconfig -> copy IPv4
+    - jalankan laravel di terminal dengan : php artisan serve --host your-ip --port 8000
+    - sisanya sama kayak ngetest di postman
+
+    test di flutter
+    - pendeknya -> liat pull request yang aku taruh di flutter_mobile
+    - panjangnya -> aku lupa 
+*/
+
 // uji coba
 Route::get('/p', function() { 
     dd('test');
 });
-
+// registrasi
 Route::post('/register', [AkunController::class, 'register']);
+// login web
 Route::post('/login1', [AkunController::class, 'loginweb']);
+// login mobile
 Route::post('/login2', [AkunController::class, 'loginmobile']);
 // Route::post('/logout', [AkunController::class, 'logout']);
-
+// buat pesanan
 Route::post('/dt', [PemesananController::class, 'create']);
 
-
+// data base
 Route::get('/bs', [BaseController::class, 'index']);
-
+// data ukuran
 Route::get('/uk', [UkuranController::class, 'index']);
-
+// data desain utama
 Route::get('/ds', [DesainController::class, 'index']);
-
+// data kue (rencana dak aku pake, langsung disatuin ke pemesanan)
 Route::get('/kue', [KueController::class, 'index']);
-
+// data pemesanan
 Route::get('/pm', [PemesananController::class, 'index']);
-
-Route::get('/ps', [PesananController::class, 'index']);
+// data pesanan all + pesanan one by id
 Route::get('/ps1', [PesananController::class, 'index']);
 Route::get('/ps0/{id}', [PesananController::class, 'show']);
 
@@ -48,11 +63,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+// Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::get('google/login/url', '\App\Http\Controllers\api\GoogleController@getAuthUrl');
